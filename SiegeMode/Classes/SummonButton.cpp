83427528139633346ce36b2e3f49e6183db6f804
Sorae <seokmin@nhnnext.org>
2016-05-23 02:55:39 +0900

    소환버튼 대폭수정

--- SiegeMode/Classes/SummonButton.cpp
+++ SiegeMode/Classes/SummonButton.cpp
@@ -2,42 +2,33 @@
 #include "SummonButton.h"
 #include "UnitManager.h"
 
-std::string SummonButton::_selectedUnitName = "";
+std::string SummonButton::g_selectedUnitName = "";
+Rect SummonButton::g_summonableZone = Rect(DEF::FIGHTING_ZONE.origin,Size(DEF::FIGHTING_ZONE.size.width/3.f,DEF::FIGHTING_ZONE.size.height));
 
 SummonButton* SummonButton::create(Vec2 pos, std::string unitName)
 {
 	auto newInst = SummonButton::create();
 	newInst->setPosition(pos);
 	newInst->setGlobalZOrder(DEF::ZORDER_UI);
-
-	auto newFrame = Sprite::create("SpriteSource/UI/unit_frame.png");
-	auto newUnit = Sprite::create("SpriteSource/UI/" + unitName + ".png");
-	auto newCover = Sprite::create("SpriteSource/UI/framecover.png");
-
-	newCover->setLocalZOrder(newFrame->getLocalZOrder() + 2);
-	newUnit->setLocalZOrder(newFrame->getLocalZOrder() + 1);
-	newInst->addChild(newFrame);
-	newInst->addChild(newUnit);
-	newInst->addChild(newCover);
-	newFrame->getTexture()->setAliasTexParameters();
-	newUnit->getTexture()->setAliasTexParameters();
-	newCover->getTexture()->setAliasTexParameters();
-
-	EventDispatcher* dispatcher = Director::getInstance()->getEventDispatcher();
-	auto listener = EventListenerTouchOneByOne::create();
-	listener->setSwallowTouches(true);
-	listener->onTouchBegan = CC_CALLBACK_2(SummonButton::onSprTouchBegan, newInst);
-	listener->onTouchEnded = CC_CALLBACK_2(SummonButton::onSprTouchEnd, newInst);
-	listener->onTouchMoved = CC_CALLBACK_2(SummonButton::onSprTouchMoved, newInst);
-	dispatcher->addEventListenerWithSceneGraphPriority(listener, newInst);
-
-	newInst->setFrameCover(newCover);
-	newInst->setFrame(newFrame);
-	newInst->setUnit(newUnit);
 	newInst->setAnchorPoint(Vec2(0.f, 0.f));
 	newInst->setUnitName(unitName);
 	newInst->scheduleUpdate();
 
+	auto tempFactory = [=](std::string fileName, int zorder) {
+		static auto zOrigin = 0;
+		auto tempSpr = Sprite::create(fileName);
+		tempSpr->setLocalZOrder(zOrigin + zorder);
+		tempSpr->getTexture()->setAliasTexParameters();
+		tempSpr->setGlobalZOrder(DEF::ZORDER_UI);
+		newInst->addChild(tempSpr);
+		return tempSpr;
+	};
+	auto newFrame = tempFactory("SpriteSource/UI/unit_frame.png", 0);
+	auto newCover = tempFactory("SpriteSource/UI/framecover.png", 2);
+	auto newUnitImage = tempFactory("SpriteSource/UI/" + unitName + ".png", 1);
+	newInst->setFrameCover(newCover);
+	newInst->setFrame(newFrame);
+	newInst->setUnitImage(newUnitImage);
 	newCover->setAnchorPoint(Vec2::ANCHOR_MIDDLE_BOTTOM);
 	newCover->setPositionY(newFrame->getContentSize().height / -2.f);
 	newCover->setOpacity(255 * 0.7f);
@@ -49,12 +40,17 @@ SummonButton* SummonButton::create(Vec2 pos, std::string unitName)
 	newInst->getUnitPreview()->getTexture()->setAliasTexParameters();
 	newInst->addChild(newInst->getUnitPreview());
 	newInst->getUnitPreview()->setFlippedX(true);
-
-
-	newUnit->setGlobalZOrder(DEF::ZORDER_UI);
-	newFrame->setGlobalZOrder(DEF::ZORDER_UI);
-	newCover->setGlobalZOrder(DEF::ZORDER_UI);
 	newInst->getUnitPreview()->setGlobalZOrder(DEF::ZORDER_UI);
+
+
+	EventDispatcher* dispatcher = Director::getInstance()->getEventDispatcher();
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->setSwallowTouches(true);
+	listener->onTouchBegan = CC_CALLBACK_2(SummonButton::onSprTouchBegan, newInst);
+	listener->onTouchEnded = CC_CALLBACK_2(SummonButton::onSprTouchEnd, newInst);
+	listener->onTouchMoved = CC_CALLBACK_2(SummonButton::onSprTouchMoved, newInst);
+	dispatcher->addEventListenerWithSceneGraphPriority(listener, newInst);
+
 	return newInst;
 }
 
@@ -68,24 +64,23 @@ void SummonButton::update(float delta)
 	else if (_unitName == "balistar")
 		coolTime = 30.f;
 
-	if (_selectedUnitName == _unitName)
+	if (g_selectedUnitName == _unitName)
 		_frame->setColor(Color3B::GREEN);
 	else
 		_frame->setColor(Color3B::WHITE);
 	if (_frameCover->getScaleY() > 0.f)
 		_frameCover->setScaleY(_frameCover->getScaleY() - delta / coolTime);
+	updateUnitPreviewColor();
 }
 
 void SummonButton::onSprTouchEnd(Touch* touch, Event* event)
 {
 	Point pos = touch->getLocation() + Vec2(0, 50);
-	if (_isSummonAble == true)
+	if (isSummonAble())
 	{
 		UnitManager::getInstance()->summonUnit(_unitName, pos, DEF::PLAYER_RED);
 		_frameCover->setScaleY(1.f);
 	}
-	_isSummonAble = false;
-	_isSummonMode = false;
 	_unitPreview->setVisible(false);
 }
 
@@ -95,91 +90,90 @@ void SummonButton::onSprTouchMoved(Touch* touch, Event* event)
 {
 	if (_isSummonMode == false)
 		return;
-	auto pos = touch->getLocation() + Vec2(0, 50);
-	Rect rect = DEF::FIGHTING_ZONE;
-	rect.size.width = DEF::SCREEN_WIDTH / 3.f;
-	if (rect.containsPoint(pos))
-		_isSummonAble = true;
-	else
-		_isSummonAble = false;
-	_unitPreview->setPosition(pos - _position + Vec2(0, 20));
-	showUnitPreview(pos);
+	updateSummonPoint(touch);
+	updateUnitPreviewColor();
+	_unitPreview->setVisible(true);
+	_unitPreview->setPosition(_summonPoint - _position + Vec2(0, 20));
 }
 
-void SummonButton::showUnitPreview(Vec2 position)
+void SummonButton::updateUnitPreviewColor()
 {
-	if (_isSummonAble == true)
+	if (isSummonAble())
 		_unitPreview->setColor(Color3B::GREEN);
+	else if (g_summonableZone.containsPoint(_summonPoint))
+		_unitPreview->setColor(Color3B::ORANGE);
 	else
 		_unitPreview->setColor(Color3B::RED);
 
+}
+
+void SummonButton::showOverlay()
+{
+	// ------- 통통 튀는 효과 ------
+	runAction(
+		EaseElasticOut::create(
+			Sequence::create(
+				ScaleTo::create(0.3f, 1.05f),
+				ScaleTo::create(0.4f, 1.00f),
+				nullptr)));
+
+	// ------ 오버레이 효과 ------
+	auto overlay = Sprite::createWithTexture(_frame->getTexture());
+
+	// 밝게 빛나는 효과를 주기 위해 블렌딩을 설정한다.
+	BlendFunc add;
+	add.dst = GL_ONE;
+	add.src = GL_SRC_ALPHA;
+	overlay->setBlendFunc(add);
+
+	overlay->runAction(
+		Sequence::create(
+			ScaleTo::create(0.4f, getScale() * 1.2f), RemoveSelf::create(true), nullptr));
+	overlay->runAction(
+		FadeOut::create(0.4f));
+	overlay->setPosition(getPosition());
+
+	getParent()->addChild(overlay);
+}
+
+bool SummonButton::isSummonAble()
+{
+	if (_frameCover->getScaleY() > 0.f)
+		return false;
+	if (!g_summonableZone.containsPoint(_summonPoint))
+		return false;
+
+	return true;
+}
+
+void SummonButton::updateSummonPoint(Touch* touch)
+{
+
+	_summonPoint = touch->getLocation() + Vec2(0, 50);
 }
 
 bool SummonButton::onSprTouchBegan(Touch* touch, Event* event)
 {
 
 	Point pos = _frame->convertToNodeSpace(touch->getLocation());
-	Rect rect = Rect(0, 0, _frame->getContentSize().width, _frame->getContentSize().height);
-	if (rect.containsPoint(pos))
+	updateSummonPoint(touch);
+	Rect currentFrameRect = Rect(0, 0, _frame->getContentSize().width, _frame->getContentSize().height);
+	if (currentFrameRect.containsPoint(pos))
 	{
-		_selectedUnitName = _unitName;
-		// ------- 통통 튀는 효과 ------
-		runAction(
-			EaseElasticOut::create(
-				Sequence::create(
-					ScaleTo::create(0.3f, 1.05f),
-					ScaleTo::create(0.4f, 1.00f),
-					nullptr)));
-
-		// ------ 오버레이 효과 ------
-		auto overlay = Sprite::createWithTexture(_frame->getTexture());
-
-		// 밝게 빛나는 효과를 주기 위해 블렌딩을 설정한다.
-		BlendFunc add;
-		add.dst = GL_ONE;
-		add.src = GL_SRC_ALPHA;
-		overlay->setBlendFunc(add);
-
-		overlay->runAction(
-			Sequence::create(
-				ScaleTo::create(0.4f, getScale() * 1.2f), RemoveSelf::create(true), nullptr));
-		overlay->runAction(
-			FadeOut::create(0.4f));
-		overlay->setPosition(getPosition());
-
-		getParent()->addChild(overlay);
-
-
-		if (_frameCover->getScaleY() > 0.f)
-			return false;
-
-		_unitPreview->setVisible(true);
-		pos = touch->getLocation() + Vec2(0, 50);
-		_unitPreview->setPosition(pos);
+		g_selectedUnitName = _unitName;
+		showOverlay();
+		_unitPreview->setPosition(_summonPoint);
 		_isSummonMode = true;
 		return true;
 	}
 	if (_frameCover->getScaleY() > 0.f)
 		return false;
 	//소환체크
-	pos = touch->getLocation() + Vec2(0, 50);
-	if (_selectedUnitName == _unitName && _frameCover->getScaleY() <= 0.f)
+	if (isSummonAble())
 	{
-
-		Rect rect = DEF::FIGHTING_ZONE;
-
-		if (rect.containsPoint(pos))
-		{
-			_unitPreview->setVisible(true);
-			_unitPreview->setPosition(pos);
-			rect.size.width = DEF::SCREEN_WIDTH / 3.f;
-			if (rect.containsPoint(pos))
-				_isSummonAble = true;
-			_isSummonMode = true;
-			return true;
-		}
+		updateUnitPreviewColor();
+		_unitPreview->setVisible(true);
+		return true;
 	}
-
-
 	return false;
 }
--- SiegeMode/Classes/SummonButton.h
+++ SiegeMode/Classes/SummonButton.h
@@ -9,15 +9,19 @@ public:
 private:
 	CREATE_FUNC(SummonButton);
 	CC_SYNTHESIZE(Sprite*, _frame, Frame);
-	CC_SYNTHESIZE(Sprite*, _unit, Unit);
+	CC_SYNTHESIZE(Sprite*, _unitImage, UnitImage);
 	CC_SYNTHESIZE(Sprite*, _frameCover, FrameCover);
 	CC_SYNTHESIZE(std::string, _unitName, UnitName);
 	CC_SYNTHESIZE(Sprite*, _unitPreview, UnitPreview);
 	bool				onSprTouchBegan(Touch* touch, Event* event);
-	static std::string	_selectedUnitName;
 	void				onSprTouchEnd(Touch* touch, Event* event);
-	bool				_isSummonAble = false;
 	bool				_isSummonMode = false;
 	void				onSprTouchMoved(Touch* touch, Event* event);
-	void				showUnitPreview(Vec2 position);
+	void				updateUnitPreviewColor();
+	void				showOverlay();
+	bool				isSummonAble();
+	Vec2				_summonPoint;
+	void				updateSummonPoint(Touch* touch);
+	static Rect			g_summonableZone;
+	static std::string	g_selectedUnitName;
 };
